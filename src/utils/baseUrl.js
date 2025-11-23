6cc802f4b9ce5b302ccab7d6067e0de56366cf79
Anit84 <81355474+Anit84@users.noreply.github.com>
2025-11-23 12:33:57 +0100

    Update baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,4 @@
 // export const baseUrl = "https://mubarak-pharmacy.herokuapp.com";
-export const baseUrl = "https://pharmacy-backend-production-3073.up.railway.app";
+//export const baseUrl = "https://pharmacy-backend-production-3073.up.railway.app";
+export const baseUrl = "https://pharmacy-backend-production-2569.up.railway.app";
 
